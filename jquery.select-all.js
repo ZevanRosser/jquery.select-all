1e7d4e1315c74217c4f56edae6802b2103da4d1c
Zevan Rosser <bendvent@gmail.com>
2012-05-12 02:52:32 -0400

    cleaning up code

--- jquery.select-all.js
+++ jquery.select-all.js
@@ -3,27 +3,21 @@
   var cache = {};
 
   var target;
+
   $.fn.selectAll = function(theTarget) {
-    
-    target = theTarget;
-    
     var elem;
-    
+    target = theTarget;
     if (cache[target]) {
       elem = cache[target].clone();
     } else {
       elem = $("<div class='select-all'>" + this.html() + "<\/div>");
-      
       if (elem.children().length == 1) {
         elem = $(elem.html());
       }
       cache[target] = elem.clone();
     }
-    
     elem.each(populate);
-    
     elem.find("*").each(populate);
-    
     return elem;
   };
 
@@ -39,47 +33,47 @@
       curr.text("");
     } else if (curr.attr("data-var")) {
       target[curr.attr("data-var")] = curr;
-    }
+    } 
   }
 
-// this is an experimental feature
+  // this is an experimental feature
+
+
   function List(elem) {
-    var self = this;
-    self._elements = [];
-    self.parent = elem.parent();
-    self.elem = elem.clone();
+    this._elements = [];
+    this.parent = elem.parent();
+    this.elem = elem.clone();
     elem.remove();
   };
-  List.prototype.get = function(index) {
-    var self = this;
-    return self._elements[index];
-  };
-  List.prototype.set = function(index, value) {
-    var self = this;
-    self._elements[index].text(value);
-    return self;
-  }
-  List.prototype.append = function(value) {
-    var self = this;
-    var elem = self.elem.clone();
-    elem.html(value);
-    self.parent.append(elem);
-    self._elements.push(elem);
-    return self;
-  };
-  List.prototype.prepend = function(value) {
-    var self = this;
-    var elem = self.elem.clone();
-    elem.html(value);
-    self.parent.prepend(elem);
-    self._elements.unshift(elem);
-    return self;
-  };
-  List.prototype.deleteAt = function(index) {
-    var self = this;
-    self._elements[index].remove();
-    self._elements.splice(index, 1);
-    return self;
+  
+  List.prototype = {
+    constructor: List,
+    get: function(index) {
+      return this._elements[index];
+    },
+    set: function(index, value) {
+      this._elements[index].text(value);
+      return this;
+    },
+    append: function(value) {
+      var elem = this.elem.clone();
+      elem.html(value);
+      this.parent.append(elem);
+      this._elements.push(elem);
+      return this;
+    },
+    prepend: function(value) {
+      var elem = this.elem.clone();
+      elem.html(value);
+      this.parent.prepend(elem);
+      this._elements.unshift(elem);
+      return this;
+    },
+    deleteAt: function(index) {
+      this._elements[index].remove();
+      this._elements.splice(index, 1);
+      return this;
+    }
   };
 
 })(jQuery);
